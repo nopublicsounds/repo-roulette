c6b4645a35520279762ff78cfdb518cc31eaca03
이찬희 <ch20041001@gmail.com>
2026-03-14 11:00:50 +0000

    TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import dotenv from 'dotenv';
 import { Client, GatewayIntentBits, Collection, Events } from 'discord.js';
+dotenv.config();
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds]
 });
 
 client.once(Events.ClientReady, () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user?.tag}!`);
 });
 
-dotenv.config();
+
 client.login(process.env.DISCORD_TOKEN);
